586f914bdd6ad60c158d4a2a6480b4513a247748
kumar bhaskar <bhaskar646@gmail.com>
2019-04-18 19:15:27 +0530

    multiply of two number

--- forceappupdate/src/main/java/com/bhaskar/forceappupdate/Player.java
+++ forceappupdate/src/main/java/com/bhaskar/forceappupdate/Player.java
@@ -1,6 +1,31 @@
 package com.bhaskar.forceappupdate;
 
 public class Player {
-    String  name;
-    String roll;
+    int a,b;
+
+    public Player(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int sqr()
+    {
+        return this.a*this.b;
+    }
 }
