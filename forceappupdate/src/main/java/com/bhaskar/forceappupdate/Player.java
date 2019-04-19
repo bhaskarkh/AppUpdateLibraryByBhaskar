118c117a5fe21173a33a78e2da2802b0df4319f4
kumar bhaskar <bhaskar646@gmail.com>
2019-04-19 13:38:18 +0530

    multi only

--- forceappupdate/src/main/java/com/bhaskar/forceappupdate/Player.java
+++ forceappupdate/src/main/java/com/bhaskar/forceappupdate/Player.java
@@ -1,31 +1,9 @@
 package com.bhaskar.forceappupdate;
 
 public class Player {
-    int a,b;
 
-    public Player(int a, int b) {
-        this.a = a;
-        this.b = b;
-    }
-
-    public int getA() {
-        return a;
-    }
-
-    public void setA(int a) {
-        this.a = a;
-    }
-
-    public int getB() {
-        return b;
-    }
-
-    public void setB(int b) {
-        this.b = b;
-    }
-
-    public int multi()
+    public int multi(int a,int b)
     {
-        return this.a*this.b;
+        return a*b;
     }
 }
